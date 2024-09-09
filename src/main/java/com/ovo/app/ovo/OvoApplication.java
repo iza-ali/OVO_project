b50b41107c9784c2bd773290f5312a9afb685c9f
Izabella <izabellaali44@gmail.com>
2024-09-09 16:37:32 +0300

    original project

---
+++ src/main/java/com/ovo/app/ovo/OvoApplication.java
@@ -0,0 +1,13 @@
+package com.ovo.app.ovo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OvoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ovo/app/ovo/OvoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ovo.app.ovo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
